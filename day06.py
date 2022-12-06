65cf0a679d93d108aa7d0343e1dfd7aa4b566b1c
Julio Ernesto Rodríguez Cabañas <julioernesto90@gmail.com>
2022-12-06 15:55:19 +0000

    Solution for day 6

---
+++ day06.py
@@ -0,0 +1,17 @@
+import input
+import re
+
+PROBLEM_NUMBER = 6
+
+def execute(is_part_two):
+    line = input.read_lines(problem_number = 6)[0]
+    return detect_first_start_of_message_position(line, sequence_size = 4 if not is_part_two else 14)
+
+def detect_first_start_of_message_position(line, sequence_size):
+    # This regex is completely unnecessary, but we were talking about regex
+    # yesterday and I wanted to use some today no matter what ¯\_(ツ)_/¯
+    sequences = re.findall("(?=(\w{" + str(sequence_size) + "}))", line)
+    return sequence_size + next(index for index, sequence in enumerate(sequences) if len(set(sequence)) == sequence_size)
+
+print("Part 1: " + str(execute(is_part_two = False)))
+print("Part 2: " + str(execute(is_part_two = True)))
